16e325f8cb3805a7e1a2650be699c7b8709732a3
Gustavo Lee <gglee89@gmail.com>
2020-10-09 16:49:06 +0900

    Feature: Add ENV file (jsconfig.json)

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,21 +3,26 @@ import { shallow } from 'enzyme';
 
 // Components
 import App from '../App';
-import CommentBox from '../CommentBox';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
   // const div = document.createElement('div');
-
   // ReactDOM.render(<App />, div);
 
   // // Looks inside the div
   // // and checks to see if the CommentBox is in there
   // expect(div.innerHTML).toContain('Comment Box');
-
   // ReactDOM.unmountComponentAtNode(div);
 
   // Using ENZYME, this makes sure we have a component 'CommentBox' in our app
   const wrapper = shallow(<App />);
 
   expect(wrapper.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+  const wrapped = shallow(<App />);
+
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
